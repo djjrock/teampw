764cd2a8de2d93162f803eead84a48310bc893e4
djjrock <jordanmetzner@gmail.com>
2024-12-25 09:50:41 -0800

    Fix TypeScript configuration and set up basic Vite + React project

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
-import { App } from './App';
-import './index.css';
-import { ThemeProvider } from './components/ThemeProvider';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
